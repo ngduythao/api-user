8d1fe81e671030e7fbb24e5703fe740a164f9228
ngduythao <ngduythao2805@outlook.com>
2019-12-26 09:01:03 +0700

    Add sample database

--- importDatabase.js
+++ importDatabase.js
@@ -14,16 +14,18 @@ const User = require('./models/User');
 const connectDB = require('./services/database');
 connectDB();
 
-const users = JSON.parse(fs.readFileSync(`${__dirname}/_database/users.json`, 'utf-8'));
-const tutors = JSON.parse(fs.readFileSync(`${__dirname}/_database_database/tutors.json`, 'utf-8'));
-const students = JSON.parse(fs.readFileSync(`${__dirname}/_database/students.json`, 'utf-8'));
-
+//const users = JSON.parse(fs.readFileSync(`${__dirname}/_database/users.json`, 'utf-8'));
+//const tutors = JSON.parse(fs.readFileSync(`${__dirname}/_database_database/tutors.json`, 'utf-8'));
+//const students = JSON.parse(fs.readFileSync(`${__dirname}/_database/students.json`, 'utf-8'));
+// const tags = JSON.parse(fs.readFileSync(`${__dirname}/_database/tags.json`, 'utf-8'));
+const specializations = JSON.parse(fs.readFileSync(`${__dirname}/_database/specializations.json`, 'utf-8'));
 
 const importData = async () => {
     try {
-        await User.create(users);
-        await Tutor.create(tutors);
-        await Student.create(students);
+        // await User.create(users);
+        // await Tutor.create(tutors);
+        // await Student.create(students);
+        // await Specialization.create(specializations);
         console.log('Data Imported...'.green.inverse);
     } catch (error) {
         console.log(error);
